4bd517de22855fecc639a1a8fd0845100e4941cc
Ynotnaexists <antonycochran@gmail.com>
2025-08-01 11:49:38 +0100

    first commit

---
+++ src/main/java/ynotnaexists/mic/mixin/accessors/SuggestionWindowGetSelection.java
@@ -0,0 +1,11 @@
+package ynotnaexists.mic.mixin.accessors;
+
+import net.minecraft.client.gui.screen.ChatInputSuggestor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ChatInputSuggestor.SuggestionWindow.class)
+public interface SuggestionWindowGetSelection {
+    @Accessor("selection")
+    int getSelection();
+}
